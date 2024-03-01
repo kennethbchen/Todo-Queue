d1b8bec57cc0ba2d7e4fa188996965d890681803
kennethbchen <kennethbchen@gmail.com>
2024-03-01 12:28:35 -0500

    Added basic styling and display modes

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
 const inputContainer = document.getElementById("input-container");
 const inputTextarea = document.getElementById("input-textarea");
-const inputButton = document.getElementById("input-button");
+const submitButton = document.getElementById("input-button");
 
 const outputContainer = document.getElementById("output-container");
 const currentTaskDisplay = document.getElementById("output-current-task");
+const editButton = document.getElementById("edit-button");
 const completeTaskButton = document.getElementById("complete-task-button");
 
 var currentTasks = [];
 
 function updateOutput() {
+
     if (currentTasks.length > 0) {
         currentTaskDisplay.textContent = currentTasks[0];
     } else {
-        currentTaskDisplay.textContent = "";
+        currentTaskDisplay.textContent = "All Done";
     }
+}
+
+function updateInput() {
+    inputTextarea.value = "";
+
+    if (currentTasks.length == 0) {
+        return;
+    }
+
+    currentTasks.forEach((item) => {
+        inputTextarea.value += item + "\n";
+    });
 }
 
 function completeTask() {
     currentTasks.shift();
-    console.log(currentTasks);
     updateOutput();
 }
 
-inputButton.onclick = () => {
+function showEditMode() {
+    inputContainer.classList.remove("hidden");
+    outputContainer.classList.add("hidden");
+}
+
+function showDisplayMode() {
+    inputContainer.classList.add("hidden");
+    outputContainer.classList.remove("hidden");
+}
+
+submitButton.onclick = () => {
+
+    if (inputTextarea.value == "") {
+        return;
+    }
+    
     currentTasks = inputTextarea.value.split("\n");
+
+    if (currentTasks.length == 0) {
+        return;
+    }
+    
     updateOutput();
+    showDisplayMode();
 
 };
 
-completeTaskButton.onclick = completeTask;
+editButton.onclick = () => {
+    updateInput();
+    showEditMode();
+};
 
-inputButton.onclick();
+completeTaskButton.onclick = completeTask;
