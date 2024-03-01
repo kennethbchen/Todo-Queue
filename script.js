4df90608c5525bb1086cb08569646538f5f6ea85
kennethbchen <kennethbchen@gmail.com>
2024-03-01 11:31:53 -0500

    Made base functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputContainer = document.getElementById("input-container");
+const inputTextarea = document.getElementById("input-textarea");
+const inputButton = document.getElementById("input-button");
+
+const outputContainer = document.getElementById("output-container");
+const currentTaskDisplay = document.getElementById("output-current-task");
+const completeTaskButton = document.getElementById("complete-task-button");
+
+var currentTasks = [];
+
+function updateOutput() {
+    if (currentTasks.length > 0) {
+        currentTaskDisplay.textContent = currentTasks[0];
+    } else {
+        currentTaskDisplay.textContent = "";
+    }
+}
+
+function completeTask() {
+    currentTasks.shift();
+    console.log(currentTasks);
+    updateOutput();
+}
+
+inputButton.onclick = () => {
+    currentTasks = inputTextarea.value.split("\n");
+    updateOutput();
+
+};
+
+completeTaskButton.onclick = completeTask;
